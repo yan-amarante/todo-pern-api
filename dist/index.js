cd403e81dfe6be1f92a7a5b6cdffe70aca9838d9
Yan Amarante <yanlimaamarantec@hotmail.com>
2023-08-07 11:58:46 -0300

    adicionado: cors na pasta dist

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,12 @@
 import Express from "express";
 import tarefasRouter from "./src/tarefas/routes.js";
 import bodyParser from "body-parser";
+import cors from "cors";
 const app = Express();
 const port = 3000;
+app.use(cors({
+    origin: "*"
+}));
 app.use(bodyParser.json());
 app.use('/api/v1/tarefas/', tarefasRouter);
 app.listen(port, () => console.log(`servidor rodando na porta ${port}`));
